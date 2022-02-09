d7b1f5014e709d33c57dc04af7e21e73ab134965
Everton Rocha <irocha10@hotmail.com>
2022-02-08 21:31:30 -0300

    Button and video effect

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,3 +5,25 @@ menu.addEventListener("click", () => {
   navbar.classList.toggle("change");
   menu.classList.toggle("change");
 });
+
+// SECTION 2
+const video = document.querySelector(".video");
+const btn = document.querySelector(".buttons i");
+const bar = document.querySelector(".video-bar");
+
+const playPause = () => {
+  if (video.paused) {
+    video.play();
+    btn.className = "far fa-pause-circle";
+    video.style.opacity = ".7";
+  } else {
+    video.pause();
+    btn.className = "far fa-play-circle";
+    video.style.opacity = ".2";
+  }
+};
+
+btn.addEventListener("click", () => {
+  playPause();
+});
+// END SECTION 2
